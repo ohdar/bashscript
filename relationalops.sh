d1cf0b71d8eb90c8fdfa94214e57456f9e89f622
Brajesh Ohdar <ohdar@live.com>
2022-08-04 17:50:09 +0530

    template file updated

---
+++ relationalops.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Purpose:
+#Version: 
+#Created Date: Thu 4 Aug 17:49:18 IST 2022
+#Modified Date:
+#Author: Brajesh Ohdar
+# START #
+# ------------------------------- #
+DELETE THIS LINE AND WRITE YOUR CODE YOUR PROGRAM HERE
+# ------------------------------- #
+# END #
+# -------------------XXXXXXX-------------------
+# Execute below commands in terminal for this file and spot the difference
+# sudo chmod +x filename.sh
+# ./filename.sh
+# ./filename.sh options if required
+# sh filename.sh
--- template-making-code.sh
+++ template-making-code.sh
@@ -10,9 +10,16 @@ echo '#Modified Date:' >> /home/brajesh/Desktop/bashscript/$file.sh
 echo '#Author: Brajesh Ohdar' >> /home/brajesh/Desktop/bashscript/$file.sh
 echo '# START #' >> /home/brajesh/Desktop/bashscript/$file.sh
 echo '# ------------------------------- #' >> /home/brajesh/Desktop/bashscript/$file.sh
-echo ""
+echo "DELETE THIS LINE AND WRITE YOUR CODE YOUR PROGRAM HERE"
 echo '# ------------------------------- #' >> /home/brajesh/Desktop/bashscript/$file.sh
 echo '# END #' >> /home/brajesh/Desktop/bashscript/$file.sh
+echo '# ---------------XXXXX---------------- #' >> /home/brajesh/Desktop/bashscript/$file.sh
+echo '# Execute below commands in terminal for this file and spot the difference' >> /home/brajesh/Desktop/bashscript/$file.sh
+echo '# sudo chmod +x filename.sh' >> /home/brajesh/Desktop/bashscript/$file.sh
+echo '# ./filename.sh' >> /home/brajesh/Desktop/bashscript/$file.sh
+echo '# ./filename.sh options if required' >> /home/brajesh/Desktop/bashscript/$file.sh
+echo '# sh filename.sh' >> /home/brajesh/Desktop/bashscript/$file.sh
+
 
 # Move this file to /bin using below command
 # chmod 777 template
