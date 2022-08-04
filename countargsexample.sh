8bdb6d5641ef3d4b95786d5a33a02767317ad14e
Brajesh Ohdar <ohdar@live.com>
2022-08-04 14:23:47 +0530

    count argument example file added

---
+++ countargsexample.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#Purpose:
+#Version: 
+#Created Date: Thu 4 Aug 14:15:50 IST 2022
+#Modified Date:
+#Author: Brajesh Ohdar
+# START #
+# ------------------------------- #
+
+if [ $# -lt 1 ]; then
+    echo "Program Usage is './scriptname.sh' options"
+else
+    echo "Program executed sucessfully"
+fi
+
+# ------------------------------- #
+
+# Execute below commands in terminal for this file and spot the difference
+# sudo chmod +x countargsecample.sh
+# ./countargsecample.sh
+# ./countargsecample.sh test
+# sh countargs.sh test
+
+# END #
