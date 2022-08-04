aa0bdc14558c53b8c6dc00649fade9c9ba6cb0cb
Brajesh Ohdar <ohdar@live.com>
2022-08-04 14:13:30 +0530

    countargs file added to bundle

---
+++ countargs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#Purpose: Counting given positional parameters.
+#Version: 1.0
+#Created Date: Thu 4 Aug 14:06:43 IST 2022
+#Modified Date:
+#Author: Brajesh Ohdar
+# START #
+# ------------------------------- #
+echo "Your current given parameters are $#"
+
+# ------------------------------- #
+
+# Execute below commands in terminal for this file and spot the difference
+# sudo chmod +x countargs.sh
+# ./countargs.sh This is test program for countargs
+# ./countargs.sh /etc/*
+# sh countargs.sh /etc/*
+
+# END #
