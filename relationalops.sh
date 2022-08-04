8e6a0f51eaa38732f41be597d3ca6fe181cd51f7
Brajesh Ohdar <ohdar@live.com>
2022-08-04 18:00:09 +0530

    arithmatic operation program added

--- relationalops.sh
+++ relationalops.sh
@@ -1,12 +1,33 @@
 #!/bin/bash
-#Purpose:
-#Version: 
+#Purpose: This file demonstrate relational operators examples
+#Version: 1.0
 #Created Date: Thu 4 Aug 17:49:18 IST 2022
 #Modified Date:
 #Author: Brajesh Ohdar
 # START #
 # ------------------------------- #
-DELETE THIS LINE AND WRITE YOUR CODE YOUR PROGRAM HERE
+
+# -lt less than >
+# -le less than or equal to =>
+# -gt greater than <
+# -ge greater than or equal to <=
+# -eq equal to =
+# -ne no equal to !=
+
+echo -e "Please provide one number: \c"
+read -r h
+echo -e "Please provide one number: \c"
+read -r g
+
+test $h -lt $g; echo "$?"
+test $h -le $g; echo "$?"
+test $h -gt $g; echo "$?"
+test $h -ge $g; echo "$?"
+test $h -eq $g; echo "$?"
+test $h -ne $g; echo "$?"
+
+
+
 # ------------------------------- #
 # END #
 # -------------------XXXXXXX-------------------
