55259feefdcc529999664fb6628bbf377a5c8d0f
Brajesh Ohdar <ohdar@live.comm>
2022-08-05 20:55:02 +0530

    template file for current directory created

---
+++ filecreatefromtemplate1-currentdirectry.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Purpose:
+#Version: 
+#Created Date: Fri 5 Aug 20:54:14 IST 2022
+#Modified Date:
+#Author: Brajesh Ohdar
+# START #
+# ------------------------------- #
+DELETE THIS LINE AND WRITE YOUR CODE YOUR PROGRAM HERE
+# ------------------------------- #
+# END #
+# ---------------XXXXX---------------- #
+# Execute below commands in terminal for this file and spot the difference
+# sudo chmod +x filename.sh
+# ./filename.sh
+# ./filename.sh options if required
+# sh filename.sh
---
+++ template-making-code-current-directory.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+echo -e "Please provide filename you want created: \c"
+read -r file
+path="$(pwd)"
+echo $path
+touch $path/$file.sh
+echo '#!/bin/bash' > $path/$file.sh
+echo '#Purpose:' >> $path/$file.sh
+echo '#Version: ' >> $path/$file.sh
+echo '#Created Date:' `date` >> $path/$file.sh
+echo '#Modified Date:' >> $path/$file.sh
+echo '#Author: Brajesh Ohdar' >> $path/$file.sh
+echo '# START #' >> $path/$file.sh
+echo '# ------------------------------- #' >> $path/$file.sh
+echo 'DELETE THIS LINE AND WRITE YOUR CODE YOUR PROGRAM HERE' >> $path/$file.sh
+echo '# ------------------------------- #' >> $path/$file.sh
+echo '# END #' >> $path/$file.sh
+echo '# ---------------XXXXX---------------- #' >> $path/$file.sh
+echo '# Execute below commands in terminal for this file and spot the difference' >> $path/$file.sh
+echo '# sudo chmod +x filename.sh' >> $path/$file.sh
+echo '# ./filename.sh' >> $path/$file.sh
+echo '# ./filename.sh options if required' >> $path/$file.sh
+echo '# sh filename.sh' >> $path/$file.sh
+
+
+# Move this file to /bin using below command
+# chmod 777 template1
+# sudo mv template /bin/
