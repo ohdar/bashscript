3f3c53bdd63fa558d8c70985c9c1ee745e3c47e2
Brajesh Ohdar <ohdar@live.comm>
2022-08-06 19:53:40 +0530

    To demonstrate for-loop-statement to ping computers using hostfile

---
+++ for-loop.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#Purpose: To demonstrate for-loop-statement to ping computers using hostfile
+#Version: 1.0
+#Created Date: Sat 6 Aug 19:38:26 IST 2022
+#Modified Date:
+#Author: Brajesh Ohdar
+# START #
+# ------------------------------- #
+#----- SYNTEX -----------
+# for ( variable ) in (Values to variable)
+# do
+#     echo "Variable"
+# done
+
+for i in `cat hostfile`
+do
+    ping -c 3 $i
+    valid=`echo $?`
+    if [ $valid -gt 1 ]; then
+        echo "$i Host is not reachable"
+    else
+        echo "$i Host is reachable"
+    fi
+done
+
+# ------------------------------- #
+# END #
+# ---------------XXXXX---------------- #
+# Execute below commands in terminal for this file and spot the difference
+# sudo chmod +x filename.sh
+# ./filename.sh
+# ./filename.sh options if required
+# sh filename.sh
